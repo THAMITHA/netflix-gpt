a90d6eb7c127c0cbbc85ff49df9deaf307d713ff
Thamitha <thamitharajput@gmail.com>
2025-06-07 19:12:20 +0530

    update navigate under onAuthStateChange

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,37 @@
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
+
 
 
 const Header = () => {
   const user = useSelector(store => store.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const handleSignOut = () => {
     signOut(auth)
-        .then(() => {
-        navigate("/")
-      })
+        .then(() => {})
       .catch((error) => {
         navigate("/error")
       });
-}
+  }
+  useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const {uid, email, displayName, photoURL} = user;
+            dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+            navigate('/browse')
+        } else {
+            // User is signed out
+            dispatch(removeUser())
+            navigate("/")
+          }
+        });
+        
+    }, []) 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-[11rem]" src='https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo'/>
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,14 +5,12 @@ import { useState, useRef } from 'react';
 import { checkValidData } from "../utils/validate"
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate()
   const name = useRef(null)
   const email = useRef(null);
   const password = useRef(null);
@@ -47,7 +45,6 @@ const Login = () => {
           // Profile updated!
           const {uid, email, displayName, photoURL} = auth.currentUser;
           dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
-          navigate("/browse")
         }).catch((error) => {
           // An error occurred
           setErrorMessage(error.message)
@@ -65,8 +62,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in 
           const user = userCredential.user;
-          console.log(user)
-          navigate("/browse")
         })
         .catch((error) => {
           const errorCode = error.code;
