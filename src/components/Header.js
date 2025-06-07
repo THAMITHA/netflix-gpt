9eff7cf3a6d12bfb43ece41d860a4af00959bc71
Thamitha <thamitharajput@gmail.com>
2025-06-07 17:25:29 +0530

    update signout condition

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,11 +19,10 @@ const Header = () => {
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-[11rem]" src='https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo'/>
-      {user && (<div className='flex'>
+      {user && <div className='flex'>
         <img className="w-12 h-12" alt="user icon" src={user?.photoURL}/>
         <button onClick={handleSignOut} className="w-12 h-12 bg-red-700 font-bold text-white">Sign Out</button>
-      </div>
-    )}
+      </div>}
     </div>
   )
 }
