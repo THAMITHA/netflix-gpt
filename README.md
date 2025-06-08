e83376aa6842a2934409263e81409d4a172ea93f
Thamitha <thamitharajput@gmail.com>
2025-06-08 15:51:31 +0530

    Add hardcoded values to constants

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 - BugFix: Sign up user displayName and profile picture update
 - BugFix: if the user is not logged in Redirect /browse to Login Page and vice-versa 
 - Unsubscribed to the onAuthStateChanged callback
+- Add hardcoded values to constants file
 - Fetch from TMDB Movies 
 
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfi
 import { auth } from '../utils/firebase';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
+import { USER_AVATAR } from '../utils/constants';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -39,7 +40,7 @@ const Login = () => {
         const user = userCredential.user;
         updateProfile(user, {
           displayName: name.current.value,
-          photoURL: "https://avatars.githubusercontent.com/u/71762248?v=4"
+          photoURL: USER_AVATAR
         })
         .then(() => {
           // Profile updated!
