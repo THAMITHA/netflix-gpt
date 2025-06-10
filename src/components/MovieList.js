64fe67103793d2e158ae44b01c78e0a8a7d1e199
Thamitha <thamitharajput@gmail.com>
2025-06-10 20:52:42 +0530

    updated css

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,10 +3,10 @@ import MovieCard from './MovieCard'
 const MovieList = ({title, movies}) => {
   console.log(movies)
   return (
-    <div>
+    <div className='px-6 bg-black'>
+      <h1 className='text-3xl py-4 text-white'>{title}</h1>
       <div className='flex overflow-x-scroll'>
-        <h1>{title}</h1>
-        <div className='flex'>{movies.map((movie)=>(
+        <div className='flex'>{movies?.map((movie)=>(
           <MovieCard key={movie.id} posterPath={movie.poster_path}/>
         ))}
           <div/></div>
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -4,16 +4,14 @@ import MovieList from './MovieList'
 const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies)
   return (
+    movies.nowPlayingMovies &&
     <div>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-      {/*
-      MovieList- Popular
-        MovieCard* n
-      MovieList- Now Playing
-      MovieList- Trending
-      MovieList- Horror 
-
-      */}</div>
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Horrer"} movies={movies.nowPlayingMovies}/>
+    </div>
   )
 }
 
